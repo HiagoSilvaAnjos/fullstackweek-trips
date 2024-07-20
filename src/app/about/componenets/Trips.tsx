de13bbc5f5e7a3721d0a905ccefc550d06282e09
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-20 20:13:26 -0300

    feat: update database schema

--- src/app/about/componenets/Trips.tsx
+++ src/app/about/componenets/Trips.tsx
@@ -1,17 +1,6 @@
-import { prisma } from "../../lib/prisma.ts";
 import React from "react";
 
-const getTrips = async () => {
-  const trips = await prisma.trip.findMany({});
-
-  return trips;
-};
-
-const Trips = async () => {
-  const data = await getTrips();
-
-  console.log({ data });
-
+const Trips = () => {
   return <div>Trips</div>;
 };
 
