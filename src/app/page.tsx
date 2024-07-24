89bd3551cc3c0bbc5a1bef2a7fee100e807d7d65
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-24 10:42:30 -0300

    feat: add quickSearch component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import QuickSearch from "./components/QuickSearch/QuickSearch";
 import TripSearch from "./components/TripSearch/TripSearch";
 
 export default function Home() {
   return (
     <div className="">
       <TripSearch />
+      <QuickSearch />
     </div>
   );
 }
