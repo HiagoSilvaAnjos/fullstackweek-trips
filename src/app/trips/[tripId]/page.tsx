b46613168480ce3ea88a17840cc1a3d994ecd838
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-24 19:30:05 -0300

    feat: add TripHighlights component

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -2,6 +2,7 @@ import { prisma } from "@/app/lib/prisma";
 import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
 import TripDescription from "./components/TripDescription";
+import TripHighlights from "./components/TripHighlights";
 
 const getTripById = async (tripId: string) => {
   return await prisma.trip.findUnique({
@@ -21,6 +22,7 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
       <TripHeader trip={trip} />
       <TripReservation trip={trip} />
       <TripDescription description={trip.description} />
+      <TripHighlights highlights={trip.highlights} />
     </div>
   );
 };
